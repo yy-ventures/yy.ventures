1280ea136e21eaf98cde602fbf2d992801edfe7c
salman-abedin <salman104alvi@gmail.com>
2021-05-04 17:25:05 +0600

    fixed logos

--- components/IncubatorPartners.vue
+++ components/IncubatorPartners.vue
@@ -2,36 +2,39 @@
 #incubator_partners
   .incubator_partners__container.faded_out
     .h5
-    img(src="/logos/yy_goshthi.png")#goshthi
+    img(src="~assets/images/logos/yy_goshthi_white.png")#goshthi
     .p YY Goshthi is a social business incubator that helps socially & environmentally conscious entrepreneurs to launch their minimum viable product and raise the needed seed capital.
   .incubator_partners__container.faded_out
     .h5 powered by
-    img(src="~assets/images/partners/NEA.jpg")
+    #netherland
+      img(src="~assets/images/logos/NEA_white.png")#netherland
     .p Netherlands Enterprise Agency encourages entrepreneurs in sustainable, agrarian, innovative and international business. It helps with grants, finding business partners, know-how and compliance with laws and regulations.
   .incubator_partners__container.faded_out
     .h5 created by
-    img(src="/logos/yy_ventures.png")#ventures
+    img(src="~assets/images/logos/yy_ventures_white.png")#ventures
     .p YY Ventures Limited builds, incubates and invests in social businesses that are moving towards a world of three zeros - zero poverty, zero unemployment and zero net carbon emission.
 </template>
 
 <style lang="sass">
 
 #incubator_partners
-  
+  background: $yy_bg
   display: grid
   @media (min-width: 601px)
-    grid-template-columns: repeat(3, auto)
-  padding: clamp(2.5em, 10vw, 5em)
-  grid-gap: 5em
+    grid-template-columns: repeat(3, 1fr)
+  // padding: clamp(2.5em, 10vw, 5em)
+  padding: calc(1.5vw + 1.5rem)
+  // grid-gap: 5em
+  grid-gap: 50px
   align-items: start
   justify-content: center
 
   .h5
-    color: $fg_dark_title
+    color: $fg_light_title
     text-transform: uppercase
     margin-bottom: 25px
   .p
-    color: $fg_dark_text
+    color: $fg_light_text
     // font-size: clamp(10px, 2vw, 12px)
 
   img
@@ -49,4 +52,5 @@
   .incubator_partners__container
     // text-align: center
     max-width: 40ch
+
 </style>
