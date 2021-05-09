1122c51f62273202c92b73b32787d303b6695567
salman-abedin <salman104alvi@gmail.com>
2021-05-09 14:21:42 +0600

    staging services

--- pages/services.vue
+++ pages/services.vue
@@ -1,17 +1,57 @@
 <template lang="pug">
-   #home-page
-      home-hero
+#home-page
+  services-hero
+  Intro
+  //- Services
+  picture-grid(:data="services")
+  previous-work
+  pre-footer
 </template>
 <script>
-import HomeHero from '~/components/home-redo/HomeHero';
+import PreFooter from '~/components/services/PreFooter';
+import ServicesHero from '~/components/services/ServicesHero';
+import Services from '~/components/services/Services';
+import PreviousWork from '~/components/services/PreviousWork';
+import PictureGrid from '~/components/PictureGrid';
+import Intro from '~/components/services/Intro';
+
+import services from '~/assets/data/services.json';
+
+import {
+  toggle_class_on_focus,
+  add_class_on_focus,
+} from '@/assets/scripts/dom_utils';
 
 export default {
   components: {
-    HomeHero,
+    Services,
+    PreviousWork,
+    PreFooter,
+    PictureGrid,
+    Intro,
+    ServicesHero,
   },
-  // mounted() {
-  //    this.$hello();
-  // },
+  data: () => ({
+    services,
+  }),
+  mounted() {
+    toggle_class_on_focus({
+      ['.highlight']: 'scale',
+    });
+    add_class_on_focus({
+      ['.faded_out']: 'fade_in',
+      ['#intro']: 'scale_down',
+    });
+  },
 };
 </script>
-<style lang="sass" scoped></style>
+<style lang="sass">
+.picture_grid
+  .benefits_grid
+    display: grid
+    @media (min-width: 601px)
+      grid-template-columns: auto auto auto auto !important
+
+    .benefits_container:first-of-type .h4
+      line-height: 1
+</style>
