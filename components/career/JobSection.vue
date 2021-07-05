f647426b49869076ff8dfdca06d0163324750fbd
salman-abedin <salman104alvi@gmail.com>
2021-07-05 14:21:05 +0600

    updated docs

--- components/career/JobSection.vue
+++ components/career/JobSection.vue
@@ -753,9 +753,7 @@
       <div class="open-position" uk-grid>
         <div class="uk-width-1-3@m">
           <div class="job-title">
-            <div class="title-bg">
-              04
-            </div>
+            <div class="title-bg">04</div>
             <h2>YY Internships</h2>
           </div>
         </div>
@@ -766,50 +764,61 @@
                 <a class="uk-accordion-title" href="#">Graphic Designer</a>
                 <div class="uk-accordion-content">
                   <p>
-                    As a Graphic Designer, you are going to be the creator of change, the creator of impact. The way you see the world will echo in your work and we value your perspective in terms of design and creativity. Your design will be the face of our organization where you will be given the freedom to enlighten us with how your vision brings ideas into beautiful communication for our brands. As you will work closely with our team full of wonders, we ensure the growth of your personal and professional credentials. If this sounds like you, we want to hear from you! 
+                    As a Graphic Designer, you are going to be the creator of
+                    change, the creator of impact. The way you see the world
+                    will echo in your work and we value your perspective in
+                    terms of design and creativity. Your design will be the face
+                    of our organization where you will be given the freedom to
+                    enlighten us with how your vision brings ideas into
+                    beautiful communication for our brands. As you will work
+                    closely with our team full of wonders, we ensure the growth
+                    of your personal and professional credentials. If this
+                    sounds like you, we want to hear from you!
                   </p>
+                  <p>Key Responsibilites:</p>
+
                   <ul>
                     <li>Create and design print and digital materials</li>
-                    <li>Adhere to brand guidelines and complete projects according to deadline</li>
+
+                    <li>
+                      Adhere to brand guidelines and complete projects according
+                      to deadline
+                    </li>
+
                     <li>Retouch and manipulate images</li>
-                    <li>Use graphic design software and work with a wide variety of media</li>
-                    <li>Collaborate with the Creative Director and Graphic Design Manager to develop design concepts</li>
-                    <li>Receive feedback from the Creative Director and Graphic Design Manager and make necessary changes</li>
-                    <li>Assemble final presentation material for printing as needed</li>
+
+                    <li>
+                      Use graphic design software and work with a wide variety
+                      of media
+                    </li>
+
+                    <li>
+                      Collaborate with the Creative Director to develop design
+                      concepts
+                    </li>
+
+                    <li>
+                      Receive feedback from the Creative Director and Graphic
+                      Design Manager and make necessary changes
+                    </li>
+
+                    <li>
+                      Assemble final presentation material for printing as
+                      needed
+                    </li>
                   </ul>
                   <h5>Other Info</h5>
                   <ul>
                     <li><strong>Length:</strong> 3 Months</li>
-                    <li><strong>Time Commitment:</strong> 30 hours/week [5 days]</li>
+                    <li>
+                      <strong>Time Commitment:</strong> 30 hours/week [5 days]
+                    </li>
                     <li><strong>Reporting to:</strong> Team Lead</li>
                     <li><strong>Remuneration:</strong> 6000 BDT/Month</li>
-                    <li><strong>Location:</strong> 9th Floor, Grameen Telecom Bhaban, Zoo Road, Mirpur 1, Dhaka 1216</li>
-                  </ul>
-                </div>
-              </li>
-              <li>
-                <a class="uk-accordion-title" href="#">Content Developer</a>
-                <div class="uk-accordion-content">
-                  <p>
-                    As a content developer, you will have the chance to bring ideas into live communication that will have both direct and indirect outreach to our valued audience and partners. You will work with a team that has the ability to bring out the best in you, so we need someone who knows their strength and weakness vividly. We are looking for someone who has the guts to think outside the box and challenge their own work as they grow into becoming a crucial part of the organization. As part of our organization, you will have the opportunity to take the brand into the next level. If this sounds like you, we want to hear from you!
-                  </p>
-                  <ul>
-                    <li>Generate topics based on daily research and current events</li>
-                    <li>Brainstorm and develop campaigns for our brands which is well planned and designed to bring specific outcome</li>
-                    <li>Collaborate with internal and/or external resources to update existing content</li>
-                    <li>Manage multiple projects concurrently and meet deliverable deadlines</li>
-                    <li>Ensure the quality of the Content & edit them as per guidelines</li>
-                    <li>Proofreading the content to provide grammatically correct text</li>
-                    <li>Receive feedback from the Creative Manager and Team Lead and make necessary changes</li>
-                    <li>Reading relevant books and going through tutorials provided by the supervisors for better output and more polished work</li>
-                  </ul>
-                  <h5>Other Info</h5>
-                  <ul>
-                    <li><strong>Length:</strong> 3 Months</li>
-                    <li><strong>Time Commitment:</strong> 30 hours/week [5 days]</li>
-                    <li><strong>Reporting to:</strong> Team Lead</li>
-                    <li><strong>Remuneration:</strong> 6000 BDT/Month</li>
-                    <li><strong>Location:</strong> 9th Floor, Grameen Telecom Bhaban, Zoo Road, Mirpur 1, Dhaka 1216</li>
+                    <li>
+                      <strong>Location:</strong> 9th Floor, Grameen Telecom
+                      Bhaban, Zoo Road, Mirpur 1, Dhaka 1216
+                    </li>
                   </ul>
                 </div>
               </li>
@@ -817,58 +826,167 @@
                 <a class="uk-accordion-title" href="#">Copywriter</a>
                 <div class="uk-accordion-content">
                   <p>
-                    As a copywriter, you will be the voice and often the first impression for many of our audience and stakeholders so it’s an understatement that you will play an integral role in shaping our brands. Here your literary art will be rising above the conventional and grabbing that persuasive trait to generate more leads than ever before. You will work hand-in-hand with a diverse team to deliver an authentic yet impactful copywriting. We don’t want someone who just takes orders. We want someone who lives and breathes ideas on branding and copywriting. If this sounds like you, we want to hear from you!
+                    As a copywriter, you will be the voice and often the first
+                    impression for many of our audience and stakeholders so it’s
+                    an understatement that you will play an integral role in
+                    shaping our brands. Here your literary art will be rising
+                    above the conventional and grabbing that persuasive trait to
+                    generate more leads than ever before. You will work
+                    hand-in-hand with a diverse team to deliver an authentic yet
+                    impactful copywriting. We don’t want someone who just takes
+                    orders. We want someone who lives and breathes ideas on
+                    branding and copywriting. If this sounds like you, we want
+                    to hear from you!
                   </p>
+                  <p>Key responsibilites:</p>
                   <ul>
-                    <li>Write original copy relevant to customer and company needs</li>
-                    <li>Write brand content for: email campaigns, homepage, landing pages, social media feeds, product descriptions, blog posts and other marketing initiative or content</li>
-                    <li>Support the team by writing and editing content for websites, blog posts, marketing materials and more</li>
-                    <li>Develop a substantial portfolio of published writing samples, refine their writing skills for different audiences, and gain valuable organizational experience</li>
-                    <li>Interviewing people supported by the organization in the social entrepreneurship and social business world and relevant stakeholders</li>
-                    <li>Proofreading the content to provide grammatically correct text</li>
-                    <li>Receive feedback from the Creative Manager and Team Lead and make necessary changes</li>
-                    <li>Reading relevant books and going through tutorials provided by the supervisors for better output and more polished work</li>
+                    <li>
+                      Write original copy relevant to customer and company
+                      needs.
+                    </li>
+
+                    <li>
+                      Write brand content for: email campaigns, homepage,
+                      landing pages, social media feeds, product descriptions,
+                      blog posts and other marketing initiative or content
+                    </li>
+
+                    <li>
+                      Support the team by writing and editing content for
+                      websites, blog posts, marketing materials and more.
+                    </li>
+
+                    <li>
+                      Develop a substantial portfolio of published writing
+                      samples, refine their writing skills for different
+                      audiences, and gain valuable organizational experience.
+                    </li>
+
+                    <li>
+                      Interviewing people supported by the organization in the
+                      social entrepreneurship and social business world and
+                      relevant stakeholders.
+                    </li>
+
+                    <li>
+                      Proofreading the content to provide grammatically correct
+                      text.
+                    </li>
+
+                    <li>Make necessary changes following feedback</li>
                   </ul>
                   <h5>Other Info</h5>
                   <ul>
                     <li><strong>Length:</strong> 3 Months</li>
-                    <li><strong>Time Commitment:</strong> 30 hours/week [5 days]</li>
+                    <li>
+                      <strong>Time Commitment:</strong> 30 hours/week [5 days]
+                    </li>
                     <li><strong>Reporting to:</strong> Team Lead</li>
                     <li><strong>Remuneration:</strong> 6000 BDT/Month</li>
-                    <li><strong>Location:</strong> 9th Floor, Grameen Telecom Bhaban, Zoo Road, Mirpur 1, Dhaka 1216</li>
+                    <li>
+                      <strong>Location:</strong> 9th Floor, Grameen Telecom
+                      Bhaban, Zoo Road, Mirpur 1, Dhaka 1216
+                    </li>
                   </ul>
                 </div>
               </li>
               <li>
-                <a class="uk-accordion-title" href="#">Web Developer</a>
+                <a class="uk-accordion-title" href="#">Video Editor</a>
                 <div class="uk-accordion-content">
                   <p>
-                    As a web developer, you will be handling and creating next generation websites with the cutting edge and trending technologies. We build websites that deliver the best user experiences and speak the stories of our clients. So our development wing always appreciates that thinker who adds value in the whole developing process. You’ll be working with a passionate team of thinkers who believe in neverending learning and efficient execution. You’ll see the impact of your work and see the growth as you grow more. So if you’re a thinker and a curious learner of modern development trends, we want to hear from you. 
+                    We are looking for an enthusiastic individual, who can grasp
+                    the idea of perfectly translating a director's idea into a
+                    beautifully crafted video piece, worthy of broadcasting. As
+                    a video intern, you will assist the
+                    photographer/videographer who captures, edits, and produces
+                    photos and videos for both internal and external purposes.
+                    In this way, you can practice your skills and learn to use
+                    multiple software that will enable you to create some
+                    extraordinary pieces.
                   </p>
+                  <p>Key responsibilites:</p>
                   <ul>
-                    <li>Assisting the team with all aspects of software design and coding</li>
-                    <li>Attending and contributing to company development meetings</li>
-                    <li>Learning the codebase and improving your coding skills</li>
-                    <li>Writing and maintaining code</li>
-                    <li>Working on minor bug fixes</li>
-                    <li>Monitoring the technical performance of internal systems</li>
-                    <li>Responding to requests from the development/design team</li>
-                    <li>Gathering information from customers about program functionality</li>
-                    <li>Writing reports</li>
-                    <li>Conducting development tests</li>
+                    <li>
+                      Assemble raw footage and transferring or uploading to a
+                      computer
+                    </li>
+
+                    <li>Follow a script, screenplay or outline</li>
+
+                    <li>
+                      Input sound to enhance footage, which may include
+                      selecting music and writing voice-overs
+                    </li>
+
+                    <li>
+                      Digitally splice film and video and synchronizing them
+                      into one rough cut file
+                    </li>
+
+                    <li>
+                      Improve and correct lighting, coloring and faulty footage
+                    </li>
                   </ul>
                   <h5>Other Info</h5>
                   <ul>
                     <li><strong>Length:</strong> 3 Months</li>
-                    <li><strong>Time Commitment:</strong> 30 hours/week [5 days]</li>
+                    <li>
+                      <strong>Time Commitment:</strong> 30 hours/week [5 days]
+                    </li>
                     <li><strong>Reporting to:</strong> Team Lead</li>
                     <li><strong>Remuneration:</strong> 6000 BDT/Month</li>
-                    <li><strong>Location:</strong> 9th Floor, Grameen Telecom Bhaban, Zoo Road, Mirpur 1, Dhaka 1216</li>
+                    <li>
+                      <strong>Location:</strong> 9th Floor, Grameen Telecom
+                      Bhaban, Zoo Road, Mirpur 1, Dhaka 1216
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <a class="uk-accordion-title" href="#">Investment Analyst</a>
+                <div class="uk-accordion-content">
+                  <p>As an Investment Analyst intern, you can engage yourself in one of the most important sectors of a company where you have to gather information, learn to interpret them and also develop data that will lead to the progress of the company. You will be required to daily assess the company or client data, analyze and interpret these data and prepare draft reports based on your analysis. </p>
+                  <ul>
+
+<li>
+Assist in research work 
+</li>
+
+<li>
+Collect data for financial modelling 
+</li>
+
+<li>
+Assist in documentation and implementation of strategic plans 
+</li>
+
+<li>
+Learn about the uses of different financial tools and implement where necessary 
+</li>
+
+                  </ul>
+                  <h5>Other Info</h5>
+                  <ul>
+                    <li><strong>Length:</strong> 3 Months</li>
+                    <li>
+                      <strong>Time Commitment:</strong> 30 hours/week [5 days]
+                    </li>
+                    <li><strong>Reporting to:</strong> Team Lead</li>
+                    <li><strong>Remuneration:</strong> 6000 BDT/Month</li>
+                    <li>
+                      <strong>Location:</strong> 9th Floor, Grameen Telecom
+                      Bhaban, Zoo Road, Mirpur 1, Dhaka 1216
+                    </li>
                   </ul>
                 </div>
               </li>
               <div class="contact-email">
-                <a class="uk-button uk-button-default apply-btn" target="_blank" href="https://airtable.com/shreMawQdjWB0sX8n">APPLY</a>
+                <a
+                  class="uk-button uk-button-default apply-btn"
+                  target="_blank"
+                  href="https://airtable.com/shrZv6CjLcmDxdXRE"
+                  >APPLY</a
+                >
               </div>
             </ul>
           </div>
