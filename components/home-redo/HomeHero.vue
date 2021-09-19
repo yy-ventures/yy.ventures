07ed57f77cf377bee4aa999dd985b27498a478a3
farhan21 <farhanabsar21@gmail.com>
2021-09-19 14:06:21 +0600

    closed

--- components/home-redo/HomeHero.vue
+++ components/home-redo/HomeHero.vue
@@ -46,9 +46,9 @@
           <!-- </h1>                                                                               -->
 
           <h1>Together We Are</h1>
-          <h1 class="write">&nbsp</h1>
-          <h2>YY Goshthi Fall 2021 Cohort Application is open.</h2>
-          <a href="https://airtable.com/shraT8HKyxXMuaG6X" class="link_button">Apply Now</a>
+          <h1 class="write">&nbsp;</h1>
+          <h2>YY Goshthi Fall 2021 Cohort Application is closed.</h2>
+          <!-- <a href="https://airtable.com/shraT8HKyxXMuaG6X" class="link_button">Apply Now</a> -->
         </div>
       </div>
     </div>
