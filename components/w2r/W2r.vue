27f84434d9f7613b69d4e9de2a423d574f0a375f
Farhan21 <farhanabsar21@gmail.com>
2021-11-26 22:43:33 +0600

    form

---
+++ components/w2r/W2r.vue
@@ -0,0 +1,520 @@
+<template>
+  <div class="solid-waste-container">
+    <div class="solid-waste-header-banner ventures-container">
+      <img src="~/assets/images/programs/waste-banner.png" alt="solid-waste" />
+    </div>
+
+    <div class="solid-waste-paragraphs ventures-container">
+      <h1 class="highlight">
+        For Solid Waste Management Entrepreneurs in Bangladesh
+      </h1>
+      <div class="solid-waste-paragraphs-container">
+        <p>
+          The United Nations Development Programme (UNDP) has teamed up with YY
+          Ventures to support 5 enterprises dedicated to building innovative
+          solution for the proper handling of unwanted, non-reusable waste and
+          finding alternatives to single-use plastics.
+        </p>
+        <p>
+          The Sustainable Solutions to Solid Waste Management (SWM) project was
+          initiated by UNDP in response to the growing Rohingya crisis in
+          Bangladesh. The Kutupalong refugee camp in Cox’s Bazar is currently
+          home to almost 900,000 Rohingya refugees making it the largest refugee
+          camp in the world. Currently, the camp generates 119.8 tons of waste
+          per day. The project aims to address the pressing SWM necessities both
+          in the host communities and refugee camps
+        </p>
+        <p>
+          Multilayer plastics (MLP), single layer plastics (SLP), wasted
+          clothes, wrappers and foil papers are considered as non-valuable
+          materials which have no demand in the local market, and as a result,
+          they end up in the drains, water bodies and landfills. However, these
+          materials have a potential to be up-cycled and recycled into different
+          products. This program gives the scope to engage enterprises to design
+          and develop prototypes to solve these problems.
+        </p>
+      </div>
+    </div>
+
+    <div class="solid-waste-prize">
+      <div class="solid-waste-prize-container">
+        <h2 class="highlight">Learning Journey in Teknaf and Ukhia Upazila</h2>
+      </div>
+      <div class="solid-waste-prize-container">
+        <h2 class="highlight">Opportunity to win grant funding of $10,000</h2>
+      </div>
+      <div class="solid-waste-prize-container">
+        <h2 class="highlight">5 Enterprises</h2>
+      </div>
+    </div>
+
+    <!-- solid waste footer -->
+    <div class="solid-waste-benefits ventures-container">
+      <h2 class="highlight">Benefits entrepreneurs will receive</h2>
+      <div class="solid-waste-benefits-container">
+        <div class="benefit-grid">
+          <div class="benefit-grid-img">
+            <img src="~/assets/images/programs/Training.jpg" alt="" />
+          </div>
+          <div class="benefit-grid-body">
+            <h4>World Class Training</h4>
+            <p>
+              This program will allow entrepreneurs to constructs their
+              innovative ideas in the presence of world reputed industry experts
+              who will guide them through an on-site beneficiary discovery
+              journey in Cox’s Bazar. Participants will also go on a day long
+              field trip to the Teknaf and Ukhia Upazila to receive hands on
+              experience, which will later allow them to prepare a market
+              assessment and develop their supply chain.
+            </p>
+          </div>
+        </div>
+        <div class="benefit-grid">
+          <div class="benefit-grid-img">
+            <img src="~/assets/images/programs/Mentoring.jpg" alt="" />
+          </div>
+          <div class="benefit-grid-body">
+            <h4>Tailored Mentoring</h4>
+            <p>
+              Selected entrepreneurs will receive one-on-one tailored mentoring
+              from a pool of seasoned professionals and industry experts, who
+              will guide them to develop their prototype, market assessment and
+              supply chain.
+            </p>
+          </div>
+        </div>
+        <div class="benefit-grid">
+          <div class="benefit-grid-img">
+            <img src="~/assets/images/programs/Funding.jpg" alt="" />
+          </div>
+          <div class="benefit-grid-body">
+            <h4>Funding</h4>
+            <p>
+              Selected enterprises will receive grant funding up to $10,000 for
+              their prototyping, market assessment and supply chain design after
+              being reviewed by a panel of judges.
+            </p>
+          </div>
+        </div>
+        <div class="benefit-grid">
+          <div class="benefit-grid-img">
+            <img src="~/assets/images/programs/A2N.jpg" alt="" />
+          </div>
+          <div class="benefit-grid-body">
+            <h4>Access to Network</h4>
+            <p>
+              Entrepreneurs will have access to a community of compassionate,
+              creative, and committed social entrepreneurs who aim to achieve
+              collective progress through collaboration and exchange. They will
+              also be able to access to our network which includes PUM
+              Netherlands, Impact Hub Network, Netherlands Enterprise Agency,
+              and the Grameen family of Social Businesses.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="looking-for">
+      <div>
+        <h2>We are looking for you</h2>
+        <p>
+          Any new business, startup or Social Business registered in Bangladesh
+          can apply for the program.
+        </p>
+      </div>
+    </div>
+    <div class="use-funding">
+      <div>
+        <h2>Where you can use the funding</h2>
+        <p>Enterprises can use the funding for the followings:</p>
+        <ul>
+          <li>
+            Purchasing machines: Machineries can be purchased with the funding.
+          </li>
+          <li>
+            Purchase tools: Safety equipment and gears, necessary tools for
+            recycling plastics can be purchased.
+          </li>
+          <li>
+            Purchasing raw materials: Funds can be used for purchasing
+            recyclable raw materials.
+          </li>
+          <li>
+            Increase manpower: Funds can be used for providing salaries to
+            employees with proper justification.
+          </li>
+          <li>
+            Sorting and storage facility: Funds can be used to build new
+            facilities to sort and store recyclables in an organised and
+            effective way.
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="not-use-funding">
+      <div>
+        <h2>Where you cannot use the funding</h2>
+        <p>
+          The following activities will not be authorised under the Program:
+        </p>
+        <ul>
+          <li>
+            Opening or duplicating current operation in other areas: The
+            recipient must not use the funding to duplicate the same operation
+            in any other location.
+          </li>
+          <li>
+            Political lobby, speed money and illegal activity: The grant funding
+            cannot be used for any sort of political lobbying, speed money or
+            illegal activity.
+          </li>
+          <li>
+            Legal activity against government & others organisations: Spending
+            funds for legal activity against the Government or other entities is
+            not allowed.
+          </li>
+          <li>
+            Pay loans or debts: No personal or organisational loan or debts can
+            be repaid with the fund.
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="innovation-solution">
+      <div>
+        <p>
+          Got a Brilliant and Innovative Solution to deal with waste without
+          value or have an
+        </p>
+        <p>alternative solution to one-single-use plastics?</p>
+        <h2>Join the Waste 2 Resources Program</h2>
+      </div>
+    </div>
+    <div class="join-now">
+      <div>
+        <div>
+          <h3 class="highlight">Interested to join?</h3>
+          <h4>Please send us your details</h4>
+        </div>
+        <!-- <div class="form">
+            <form>
+                <div class="form-grid">
+                    <div>
+                        <input type="text" name="firstName" v-model="firstName" placeholder="first name" required>
+                    </div>
+                    <div>
+                        <input type="text" name="lastName" v-model="lastName" placeholder="last name" required>
+                    </div>
+                </div>
+                <div class="form-grid">
+                    <div>
+                        <input type="email" name="email" v-model="email" placeholder="email" required>
+                    </div>
+                    <div>
+                        <input type="text" name="number" v-model="number" placeholder="contact number" required>
+                    </div>
+                </div>
+                <div>
+                    <input type="text" placeholder="Name of Enterprise">
+                </div>
+                <div>
+                    <p>Tell us about your business model (300 words)*</p>
+                    <textarea name="message" v-model="message"></textarea>
+                </div>
+                <div>
+                    <h5>Attach your pitch deck </h5>
+                    <input type="file" name="file"/>
+                    <h6>*Your pitch deck should include: Problem statement, Market Opportunity, Solution, Service/Product, Business Model, Revenue Structure, Roadmap, Impact & Team.</h6>
+                </div>
+            </form>
+        </div> -->
+        <div class="apply-now">
+          <a
+            href="https://docs.google.com/forms/d/17QMsOfodo--QqmlbQomHtwKhh_yrQbtcH7ulWDez900/edit"
+            >Apply Now</a
+          >
+        </div>
+        <p>
+          For information or queries about the program please reach out to
+          <span class="join-bold">sharika@yy.ventures</span>.
+        </p>
+      </div>
+    </div>
+    <div class="partner">
+      <div>
+        <h2 class="highlight">partner</h2>
+        <div>
+            <img src="~/assets/images/programs/undp.png" alt="" />
+        </div>
+      </div>
+    </div>
+    <!-- solid waste footer -->
+    <div class="solid-waste-footer"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.ventures-container {
+  width: 80%;
+  margin: 0 auto;
+}
+.solid-waste-container {
+  padding: 2rem 0 0 0;
+  .solid-waste-header-banner {
+    margin-top: 5rem;
+    img {
+      width: 100%;
+      height: auto;
+      object-fit: cover;
+    }
+  }
+  h1 {
+    text-transform: capitalize;
+    margin-top: 4rem;
+    font-size: 8vmin;
+  }
+  .solid-waste-paragraphs {
+    margin-top: 2rem;
+    .solid-waste-paragraphs-container {
+      margin-top: 2rem;
+    }
+  }
+  .solid-waste-prize {
+    padding: 5% 10%;
+    background-color: #eee;
+    margin-top: 4rem;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 5%;
+    .solid-waste-prize-container {
+      h2 {
+        text-transform: uppercase;
+        font-size: 4vmin;
+        font-weight: 900;
+      }
+    }
+  }
+
+  .solid-waste-benefits {
+    margin-top: 4rem;
+    h2 {
+      text-transform: capitalize;
+      font-size: 6vmin;
+      font-weight: 900;
+    }
+    .solid-waste-benefits-container {
+      margin-top: 4rem;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 2%;
+      @media screen and (max-width: 601px) {
+        grid-template-columns: 1fr;
+      }
+      .benefit-grid {
+        .benefit-grid-img {
+          width: 100%;
+        }
+        .benefit-grid-body {
+          padding: 5% 0;
+          min-height: 350px;
+          @media screen and (max-width: 601px) {
+            min-height: auto;
+          }
+          h4 {
+            font-weight: 900;
+            text-transform: capitalize;
+            font-size: 3.2vmin;
+          }
+        }
+      }
+    }
+  }
+  .looking-for {
+    margin-top: 2rem;
+    padding: 5% 10%;
+    background: #c7265b;
+    color: #fff;
+    @media screen and (max-width: 601px) {
+      margin-top: 8rem;
+    }
+    h2 {
+      font-weight: 900;
+      color: #fff;
+      text-transform: capitalize;
+      font-size: 3rem;
+    }
+    p {
+      font-weight: 500;
+      text-transform: capitalize;
+    }
+  }
+  .use-funding {
+    padding: 5% 10%;
+    background: #2ba4c5;
+    h2 {
+      color: #fff;
+      font-weight: 900;
+      text-transform: capitalize;
+      font-size: 3rem;
+    }
+    p {
+      color: #fff;
+      font-size: 1.2rem;
+      font-weight: 900;
+    }
+    ul {
+      li {
+        color: #fff;
+        margin-top: 0.5rem;
+        list-style-type: circle;
+      }
+    }
+  }
+  .not-use-funding {
+    padding: 5% 10%;
+    background: #f8d23a;
+    h2 {
+      color: #444;
+      font-weight: 900;
+      text-transform: capitalize;
+      font-size: 3rem;
+    }
+    p {
+      color: #444;
+      font-size: 1.2rem;
+      font-weight: 900;
+    }
+    ul {
+      li {
+        color: #444;
+        margin-top: 0.5rem;
+        list-style-type: circle;
+      }
+    }
+  }
+  .innovation-solution {
+    padding: 5% 10%;
+    p {
+      color: #c7265b;
+      font-size: 1.2rem;
+      font-weight: 700;
+      text-transform: capitalize;
+    }
+    h2 {
+      color: #c7265b;
+      font-size: 3rem;
+      font-weight: 900;
+      text-transform: capitalize;
+    }
+  }
+  .join-now {
+    padding: 5% 10%;
+    background: #eee;
+    text-align: center;
+    h3 {
+      color: #444;
+      font-weight: 900;
+      text-transform: capitalize;
+      font-size: 2rem;
+    }
+    h4{
+        color: #444;
+      font-weight: 900;
+      text-transform: capitalize;
+      font-size: 1.5rem; 
+    }
+    p {
+      margin-top: 2rem;
+      color: #c7265b;
+      font-size: 18px;
+      .join-bold{
+          font-weight: 900;
+      }
+    }
+    .form{
+        display: flex;
+        justify-content: center;
+        form{
+            width: 50%;
+            @media screen and (max-width: 601px) {
+                width: 100%;
+            }
+        }
+        input, textarea{
+            outline: none;
+            border: 0;
+            background-color: #fff;
+            padding: 15px 20px;
+            font-size: 16px;
+            color: #444;
+            width: 100%;
+            margin-top: 1rem;
+            border-bottom: 2px solid #fff;
+            transition: 0.3s;
+            &:focus{
+                background-color: #eee;
+                border-bottom: 2px solid #444;
+            }
+        }
+        textarea{
+          min-height: 200px;
+        }
+        p{
+          text-align: left;
+          color: #c7265b;
+          font-weight: 700;
+          font-size: 14px;
+        }
+        h5{
+          margin-top: 2rem;
+          text-align: left;
+          color: #c7265b;
+          font-weight: 700;
+          font-size: 16px;
+        }
+        h6{
+          font-style: italic;
+          color: #c7265b;
+        }
+        .form-grid{
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: 2%;
+            @media screen and (max-width: 601px) {
+                grid-template-columns: 1fr;
+            }
+        }
+    }
+    .apply-now {
+      margin-top: 3rem;
+      a {
+        background: #f8d23a;
+        color: #444;
+        padding: 0.8rem 2rem;
+        cursor: pointer;
+        border-radius: 15px;
+        font-weight: 700;
+      }
+    }
+  }
+  .partner {
+    padding: 5%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    h2 {
+      font-weight: 900;
+      font-size: 4rem;
+      text-transform: capitalize;
+    }
+    img {
+      width: 25%;
+    }
+  }
+}
+</style>
