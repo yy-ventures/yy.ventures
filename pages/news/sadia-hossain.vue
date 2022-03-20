c0f6a7946ef525691f11daa3414d088a4ff5889a
farhan21 <farhanabsar21@gmail.com>
2022-03-20 13:03:02 +0600

    changes

--- pages/news/sadia-hossain.vue
+++ pages/news/sadia-hossain.vue
@@ -14,14 +14,19 @@ export default{
     title: 'Sadia Hossain Joins YY Ventures Limited as the Chief Operating Officer (COO)',
     meta: [
       {
-        hid: 'description',
-        name: 'description',
+        hid: 'og:title', 
+        name: 'og:title', 
+        content:  'Sadia Hossain Joins YY Ventures Limited as the Chief Operating Officer (COO)' 
+      },
+      {
+        hid: 'og:description',
+        name: 'og:description',
         content: 'YY Ventures Limited has onboarded Sadia Hossain as the Chief Operating Officer (COO) on March 01, 2022. Sadia is a business development and talent acquisition expert who brings in 10+ years of leadership experience into the company.'
       },
       { 
         hid: 'og:image', 
         property: 'og:image', 
-        content: '/sadia_1.jpg' 
+        content: '/sadia_2.jpg' 
       }
     ],
   }
