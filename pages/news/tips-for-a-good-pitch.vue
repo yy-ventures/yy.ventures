70535ff860fb3863445e7b3f6870081bc363780f
farhan21 <farhanabsar21@gmail.com>
2022-03-27 10:32:15 +0600

    akki meta

--- pages/news/tips-for-a-good-pitch.vue
+++ pages/news/tips-for-a-good-pitch.vue
@@ -9,6 +9,26 @@ import GoodPitch from "~/components/news/GoodPitch.vue"
 export default{
   components: {
     GoodPitch,
+  },
+  head: {
+    title: 'Tips for a good pitch',
+    meta: [
+      {
+        hid: 'og:title', 
+        name: 'og:title', 
+        content:  'Tips for a good pitch' 
+      },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content: 'I have often been asked to judge pitches of start-ups. In a good pitch the entrepreneur explains what the problem is the start-up focuses on and how she or he wants to solve it. In this context I always advise that for a good pitch to work, make it personal.'
+      },
+      { 
+        hid: 'og:image', 
+        property: 'og:image', 
+        content: '/okki-bg.jpg' 
+      }
+    ],
   }
 }
 </script>
